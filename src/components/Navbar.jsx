f1b5705dc6e24ffccd536fc6e0102748695bcdb9
Pritom Parashar <105374585+prtm737@users.noreply.github.com>
2022-06-22 23:50:50 +0530

    Update Navbar.jsx
    Name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export default function Navbar() {
   };
   return (
     <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto p-4">
-      <h1 className="w-full text-4xl font-bold text-[#4ADE80]">PRITOMPARASHAR.</h1>
+      <h1 className="w-full text-4xl font-bold text-[#4ADE80]">PRITOM.</h1>
       <ul className="hidden md:flex">
         <li className="p-4 cursor-pointer hover:scale-105 duration-200 capitalize font-medium">
         
@@ -46,7 +46,7 @@ export default function Navbar() {
         }
       >
         <h1 className="w-full text-3xl font-bold text-[#4ADE80] m-8">
-        PRITOMPARASHAR.
+        PRITOM.
         </h1>
         <ul className=" uppercase p-4">
           <li className="p-4 border-b border-b-gray-600"><Link  to="Hero" spy={true} smooth={true} offset={50} duration={500}>Home</Link></li>
