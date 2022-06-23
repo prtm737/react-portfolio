caa0787ecca2810392a672bfd79820afbaf13384
Pritom Parashar <105374585+prtm737@users.noreply.github.com>
2022-06-23 21:59:58 +0530

    Update Contact.jsx
    Height full

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name="Contact" className="w-full h-screen text-white">
-          <div className="mt-20 max-w-screen-lg p-6 flex flex-col mx-auto justify-center w-full ">
+          <div className="mt-20 max-w-screen-lg p-6 flex flex-col mx-auto justify-center w-full h-full ">
               <div className="pb-5">
                   <p className="text-5xl text-center text-[#4ADE80] font-bold border-b-4 border-[#1E293B]">Contact</p>
                   <p className='py-6 text-xl text-center'>Submit the form to contact me</p>
